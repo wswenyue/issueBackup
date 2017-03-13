78109de866983d3b81eb1e9e12e2f090b74e1014
wswenyue <wswenyue@163.com>
2017-03-13 15:34:32 +0800

    fix bug

--- backup.sh
+++ backup.sh
@@ -4,5 +4,5 @@ echo 请输入您的github用户名
 read username
 echo 请输入您的项目名字
 read repo
-wget https://api.github.com/repos/$username/$repos/issues
-wget https://api.github.com/repos/$username/$repos/issues/comments
+wget https://api.github.com/repos/$username/$repo/issues
+wget https://api.github.com/repos/$username/$repo/issues/comments
